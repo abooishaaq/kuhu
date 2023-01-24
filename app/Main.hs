30469d2452f11f6699e25ece187a3d3a8093f639
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-01-24 12:06:43 +0530

    dont need Hindley Milner for this

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,7 @@ import Parser (parseToplevel, toplevel)
 import System.Environment (getArgs)
 import Text.Pretty.Simple (pPrint)
 
-import Check (runTopLevelTys)
 import Env (empty)
-import Infer (initInfer)
 
 main :: IO ()
 main = do
@@ -16,8 +14,5 @@ main = do
             readFile name >>= \contents -> do
                 case parseToplevel contents of
                     Left err -> print err
-                    Right toplevel -> do
-                        pPrint toplevel
-                        pPrint $ runTopLevelTys toplevel
-                        return ()
+                    Right toplevel -> pPrint toplevel
         _ -> putStrLn "Usage: kuhu <filename>"
